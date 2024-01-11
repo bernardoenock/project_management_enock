4a5b7285370524508bafe1d9196a4cf4dc7e34a8
bernardoenock <bernardoenockgm@gmail.com>
2024-01-11 15:48:01 -0300

    feat: Start homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,12 +1,23 @@
+import { Typography, Button, Container, Box } from "@mui/material"
 import Link from "next/link"
 
 export default function Home() {
   return (
-    <>
-    <h1>Project Management Enock</h1>
-
-    <Link href="/dashboard">Começar</Link>
-    
-    </>
+    <Container component="main" maxWidth="xs">
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px;'
+          }}
+        >
+        <Typography variant="h4">Gerencie seus projetos</Typography>
+        <Button variant="contained">
+            <Link href="/dashboard">Aqui</Link>
+        </Button>
+      </Box>
+    </Container>
   )
 }
