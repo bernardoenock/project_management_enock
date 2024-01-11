cd22208547c3478baa36c46b4aa5e6c1c91a9064
bernardoenock <bernardoenockgm@gmail.com>
2024-01-10 21:35:33 -0300

    feat: Create mock projects

---
+++ client/src/data/mockProjects.ts
@@ -0,0 +1,75 @@
+import { generateRandomId } from "./utils";
+
+
+export const mockProjects = [
+  {
+    id: generateRandomId(),
+    name: "Projeto 1",
+    description: "Descrição do Projeto 1",
+    startDate: new Date("2024-01-10"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 2",
+    description: "Descrição do Projeto 2",
+    startDate: new Date("2024-01-11"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 3",
+    description: "Descrição do Projeto 3",
+    startDate: new Date("2024-01-12"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 4",
+    description: "Descrição do Projeto 4",
+    startDate: new Date("2024-01-13"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 5",
+    description: "Descrição do Projeto 5",
+    startDate: new Date("2024-01-14"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 6",
+    description: "Descrição do Projeto 6",
+    startDate: new Date("2024-01-15"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 7",
+    description: "Descrição do Projeto 7",
+    startDate: new Date("2024-01-16"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 8",
+    description: "Descrição do Projeto 8",
+    startDate: new Date("2024-01-17"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 9",
+    description: "Descrição do Projeto 9",
+    startDate: new Date("2024-01-18"),
+    tasks: []
+  },
+  {
+    id: generateRandomId(),
+    name: "Projeto 10",
+    description: "Descrição do Projeto 10",
+    startDate: new Date("2024-01-19"),
+    tasks: []
+  },
+];
