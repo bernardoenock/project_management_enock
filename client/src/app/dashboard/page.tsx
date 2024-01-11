54a006159c4d8d4deafd236fba36df7aeb904486
bernardoenock <bernardoenockgm@gmail.com>
2024-01-11 17:07:44 -0300

    feat: Start list projects

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,18 @@
+"use client"
 import Link from "next/link"
+import { getProjects } from "@/api/projects";
+import { useEffect } from "react"
 
 export default function Dashboard() {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const projects = await getProjects();
+      console.log('Projects:', projects);
+    }
+    fetchData();
+  }, [])
+
   return (
     <>
       <h1>Dashboard</h1>
