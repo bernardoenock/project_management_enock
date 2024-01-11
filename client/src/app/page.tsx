ebc86ee8c4869468636e9a098ffb78f96da6eb02
bernardoenock <bernardoenockgm@gmail.com>
2024-01-10 21:35:04 -0300

    feat: Start create a home page

---
+++ client/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <>
+    <h1>Project Management Enock</h1>
+
+    <Link href="/dashboard">Começar</Link>
+    
+    </>
+  )
+}
