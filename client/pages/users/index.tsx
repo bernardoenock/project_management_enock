3f3644060e35047683ab01543ceac07d5f3d9af1
bernardoenock <bernardoenockgm@gmail.com>
2024-01-12 13:11:04 -0300

    feat: Users or Members Extra

---
+++ client/pages/users/index.tsx
@@ -0,0 +1,28 @@
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+
+import { User } from '../../interfaces'
+import { sampleUserData } from '../../utils/sample-data'
+import Layout from '../../components/Layout'
+import List from '../../components/List'
+
+type Props = {
+  items: User[]
+}
+
+const WithStaticProps = ({ items }: Props) => (
+  <Layout title="Users List | Project Management">
+    <h1>Users List</h1>
+    <List items={items} />
+    <p>
+      <Link href="/">Go home</Link>
+    </p>
+  </Layout>
+)
+
+export const getStaticProps: GetStaticProps = async () => {
+  const items: User[] = sampleUserData
+  return { props: { items } }
+}
+
+export default WithStaticProps
