57638161193186577f45e899951b698192fbb0d1
bernardoenock <bernardoenockgm@gmail.com>
2024-01-13 21:03:12 -0300

    feat: Create List details of Project

--- client/components/ListDetail.tsx
+++ client/components/ListDetail.tsx
@@ -1,16 +1,34 @@
 import * as React from 'react'
-
-import { User } from '../interfaces'
+import { Box, Typography } from '@mui/material'
+import CardTask from './molecules/CardTask'
+import CreateTask from './organisms/CreateTask'
+import UpdateProject from './organisms/UpdateProject'
 
 type ListDetailProps = {
-  item: User
+  item: any
 }
 
-const ListDetail = ({ item: user }: ListDetailProps) => (
-  <div>
-    <h1>Detail for {user.name}</h1>
-    <p>ID: {user.id}</p>
-  </div>
-)
+const ListDetail = ({ item }: ListDetailProps) => {
+  const [tasks, setTasks] = React.useState(item.tasks);
+
+  const handleDeleteTask = (taskId: string) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== taskId));
+  };
+  
+
+return (
+    <Box>
+      <Typography variant='h5'>{item.name}</Typography>
+      <Typography variant='h6'>{item.description}</Typography>
+      <UpdateProject productId={item.id}/>
+      <CreateTask productId={item.id}/>
+      {tasks && tasks.map((task) => (
+          <li key={task.id} style={{listStyle: 'none', margin: 0, padding: 0}}>
+            <CardTask task={task} projectId={item.id} onDelete={handleDeleteTask}/>
+          </li>
+        ))}
+    </Box>
+  )
+}
 
 export default ListDetail
