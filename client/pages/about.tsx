0d0f600fcbb0065303de1f4a0b9c9149b9a189f8
bernardoenock <bernardoenockgm@gmail.com>
2024-01-13 21:06:21 -0300

    fix: change to LinkApp

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import LinkApp from '../components/atoms/LinkApp'
 import Layout from '../components/Layout'
 
 const AboutPage = () => (
@@ -6,7 +6,7 @@ const AboutPage = () => (
     <h1>About</h1>
     <p>This is the about page</p>
     <p>
-      <Link href="/">Go home</Link>
+      <LinkApp href="/">Go home</LinkApp>
     </p>
   </Layout>
 )
