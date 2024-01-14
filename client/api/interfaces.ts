dc575cdb61d1bd7ebb10e6cc6e173882a5bf9692
bernardoenock <bernardoenockgm@gmail.com>
2024-01-13 21:02:22 -0300

    feat: Add some interfaces

--- client/api/interfaces.ts
+++ client/api/interfaces.ts
@@ -1,15 +1,23 @@
 export interface Create {
   name: string, 
   description: string, 
-  startDate: Date 
+  startDate: Date | string
 }
 
 export interface Update {
   name?: string, 
   description?: string, 
-  startDate?: Date 
+  startdate?: Date 
 }
 
 export interface CompleteTask {
   completed: boolean
+}
+
+export interface ProjectObj {
+  id: string,
+  name: string, 
+  description: string, 
+  startdate: Date 
+  tasks?: any[]
 }
