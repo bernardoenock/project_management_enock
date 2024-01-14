af59f9566477bcea725f4bcfe10905e9ce9a1f71
bernardoenock <bernardoenockgm@gmail.com>
2024-01-14 16:03:01 -0300

    feat: Refactor

--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -1,22 +1,13 @@
-import { GetStaticProps } from 'next'
-import { getProjects } from '../../api/projects'
 import Layout from '../../components/Layout'
-import List from '../../components/ListProjects'
+import ListProjects from '../../components/ListProjects'
 
-type Props = {
-  items: any[]
-}
-
-const Dashboard = ({ items }: Props) => (
+const Dashboard = () => (
   <Layout title="Projects">
     <h1>Projects List</h1>
-    <List items={items} />
+    <ListProjects />
   </Layout>
 )
 
-export const getStaticProps: GetStaticProps = async () => {
-  const items: any[] = await getProjects()
-  return { props: { items } }
-}
+
 
 export default Dashboard
