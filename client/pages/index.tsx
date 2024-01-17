eb9ca3279f63a4d5f5581712b41ba4f8a836238e
bernardoenock <bernardoenockgm@gmail.com>
2024-01-16 22:05:54 -0300

    fix: index principal page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,9 @@
-import LinkApp from '../components/atoms/LinkApp'
 import Layout from '../components/Layout'
-import { Typography, Button, Box, Container} from '@mui/material'
+import HomePage from '../components/templates/HomePage'
 
 const IndexPage = () => (
   <Layout title="Home | Project Management">
-    <Container component="main" maxWidth="xs">
-      <Box sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}>
-        <Typography variant="h4">Gerencie seus projetos</Typography>
-        <Button color="inherit" variant='contained'><LinkApp href="/dashboard">Aqui</LinkApp></Button>
-      </Box>
-    </Container>
+    <HomePage />
   </Layout>
 )
 
